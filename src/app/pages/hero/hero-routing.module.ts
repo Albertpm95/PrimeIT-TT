e2088c8a21543679b529334cf7dc654acf1fef33
Albert <albertpm95@outlook.com>
2023-02-28 11:41:26 +0100

    Renamed route

--- src/app/pages/hero/hero-routing.module.ts
+++ src/app/pages/hero/hero-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListComponent } from './list/list.component';
+import { HeroListComponent } from './list/list.component';
 
 
-const routes: Routes = [{ path: 'list', component: ListComponent }];
+const routes: Routes = [{ path: 'list', component: HeroListComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
