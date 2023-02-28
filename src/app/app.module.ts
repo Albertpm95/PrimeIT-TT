3d01856d4a25e7d938df2d364b656f7532a64a89
Albert <albertpm95@outlook.com>
2023-02-28 11:15:27 +0100

    Added app routing and components module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
@@ -10,6 +11,8 @@ import { ComponentsModule } from './components/components.module';
   ],
   imports: [
     BrowserModule,
+    ComponentsModule,
+    AppRoutingModule,
     ComponentsModule
   ],
   providers: [],
